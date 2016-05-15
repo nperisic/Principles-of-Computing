a439318d757d664aecc773b9074bc862728205d1
Nikola Perisic <nikola@perisic>
2016-05-16 00:04:49 +0200

    added test file

--- merge_2048_game.py
+++ merge_2048_game.py
@@ -27,10 +27,3 @@ def merge(line):
         l_no += 1
     return sort
 
-## test
-##print merge([2])
-##print merge([2,0,2,2])
-##print merge([2,8,2,2])
-##print merge([2,8,2,2])
-##print merge([16,8,2,2])
-##print merge([16,8,32,8,8,2,4,4,8])
